ac4182b734a15f7c31436f055d5e54393a19f291
Thomas Huang <vanlicht@gmail.com>
2019-01-08 21:01:51 -0600

    Initial commit

---
+++ app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
@@ -0,0 +1,63 @@
+package com.londonappbrewery.quizzler;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    // TODO: Declare constants here
+
+
+    // TODO: Declare member variables here:
+    Button mTrueButton;
+    Button mFalseButton;
+
+
+    // TODO: Uncomment to create question bank
+//    private TrueFalse[] mQuestionBank = new TrueFalse[] {
+//            new TrueFalse(R.string.question_1, true),
+//            new TrueFalse(R.string.question_2, true),
+//            new TrueFalse(R.string.question_3, true),
+//            new TrueFalse(R.string.question_4, true),
+//            new TrueFalse(R.string.question_5, true),
+//            new TrueFalse(R.string.question_6, false),
+//            new TrueFalse(R.string.question_7, true),
+//            new TrueFalse(R.string.question_8, false),
+//            new TrueFalse(R.string.question_9, true),
+//            new TrueFalse(R.string.question_10, true),
+//            new TrueFalse(R.string.question_11, false),
+//            new TrueFalse(R.string.question_12, false),
+//            new TrueFalse(R.string.question_13,true)
+//    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTrueButton = findViewById(R.id.true_button);
+        mFalseButton = findViewById(R.id.false_button);
+
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Quiz", "True Button Pressed!");
+                Toast myToast = Toast.makeText(getApplicationContext(),"True Button Pressed", Toast.LENGTH_SHORT);
+                myToast.show();
+
+            }
+        });
+
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Quiz", "False Button Pressed!");
+                Toast.makeText(getApplicationContext(), "False Button Pressed", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
